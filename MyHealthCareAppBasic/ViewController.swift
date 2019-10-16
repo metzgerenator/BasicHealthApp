a24ab869cb03b20e057e0e0e74d60ef0609565ca
metzgerenator <metzgerenator@gmail.com>
2019-10-15 22:08:35 -0400

    Initial Commit

---
+++ MyHealthCareAppBasic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyHealthCareAppBasic
+//
+//  Created by Mike on 10/15/19.
+//  Copyright © 2019 Mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
